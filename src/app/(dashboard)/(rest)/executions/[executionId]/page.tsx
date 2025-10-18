5e1786995a0492a37d4ab46e69012386bd21de17
rudravashishtha <rudraturtle7@gmail.com>
2025-10-18 23:54:49 +0530

    Setup Sidebar

---
+++ src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
@@ -0,0 +1,16 @@
+import { requireAuth } from "@/lib/auth-utils";
+import React from "react";
+
+interface PageProps {
+  params: Promise<{
+    executionId: string;
+  }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  await requireAuth();
+  const { executionId } = await params;
+  return <div>Execution Id: {executionId}</div>;
+};
+
+export default Page;
---
+++ src/app/(dashboard)/(rest)/executions/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { requireAuth } from "@/lib/auth-utils";
+
+const Page = async () => {
+  await requireAuth();
+  return <div>Executions</div>;
+};
+
+export default Page;
