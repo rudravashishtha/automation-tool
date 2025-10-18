f9c389db38dad3702c4f66171290f6f200783adb
tripwithrudra <rudra.vashishtha@tripjack.com>
2025-10-18 19:16:39 +0530

    Intro to background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,20 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    const { auth } = ctx;
-
-    return prisma.user.findMany({
-      where: {
-        id: auth?.user?.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "test@example.com",
+      },
+    });
+    return prisma.workflow.create({
+      data: {
+        name: "Test Workflow",
       },
     });
   }),
