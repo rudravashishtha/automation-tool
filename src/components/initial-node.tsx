4669cbc38a3796c09ddd6345028e992e09991695
rudravashishtha <rudraturtle7@gmail.com>
2025-10-22 02:35:52 +0530

    Add manual and http nodes and node selectors

--- src/components/initial-node.tsx
+++ src/components/initial-node.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import { memo } from "react";
+import { memo, useState } from "react";
 import type { NodeProps } from "@xyflow/react";
 import { PlaceholderNode } from "./react-flow/placeholder-node";
 import { PlusIcon } from "lucide-react";
 import { WorkflowNode } from "./workflow-node";
+import { NodeSelector } from "./node-selector";
 
 export const InitialNode = memo((props: NodeProps) => {
+  const [selectorOpen, setSelectorOpen] = useState(false);
   return (
-    <WorkflowNode showToolbar={false}>
-      <PlaceholderNode {...props} handleClick={() => {}}>
-        <div className="cursor-pointer flex items-center justify-center">
-          <PlusIcon className="size-4" />
-        </div>
-      </PlaceholderNode>
-    </WorkflowNode>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <WorkflowNode showToolbar={false}>
+        <PlaceholderNode {...props} handleClick={() => setSelectorOpen(true)}>
+          <div className="cursor-pointer flex items-center justify-center">
+            <PlusIcon className="size-4" />
+          </div>
+        </PlaceholderNode>
+      </WorkflowNode>
+    </NodeSelector>
   );
 });
 
--- src/features/editor/components/add-node-button.tsx
+++ src/features/editor/components/add-node-button.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import { memo } from "react";
+import { memo, useState } from "react";
 import { PlusIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { NodeSelector } from "@/components/node-selector";
 
 export const AddNodeButton = memo(() => {
+  const [selectorOpen, setSelectorOpen] = useState(false);
   return (
-    <Button
-      className="bg-background"
-      size="icon"
-      variant="outline"
-      onClick={() => {}}
-    >
-      <PlusIcon />
-    </Button>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <Button
+        className="bg-background"
+        size="icon"
+        variant="outline"
+        onClick={() => {}}
+      >
+        <PlusIcon />
+      </Button>
+    </NodeSelector>
   );
 });
 
