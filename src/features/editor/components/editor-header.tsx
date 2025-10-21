5b16876976c6ab9ae20d8ef3d52db2494822e53b
tripwithrudra <rudra.vashishtha@tripjack.com>
2025-10-21 22:29:02 +0530

    Workflow - Header and Breadcrumbs

---
+++ src/features/editor/components/editor-header.tsx
@@ -0,0 +1,138 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { SaveIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import {
+  useRenameWorkflow,
+  useSuspenseWorkflow,
+} from "@/features/workflows/hooks/use-workflows";
+import { Input } from "@/components/ui/input";
+
+export const RenameEditor = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+  const updateWorkflowName = useRenameWorkflow();
+
+  const [name, setName] = useState(workflow.name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSave = async () => {
+    if (name === workflow.name) {
+      setIsEditing(false);
+      return;
+    }
+
+    try {
+      await updateWorkflowName.mutateAsync({
+        id: workflowId,
+        name,
+      });
+    } catch (error) {
+      setName(workflow.name);
+    } finally {
+      setIsEditing(false);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSave();
+    } else if (e.key === "Escape") {
+      setName(workflow.name);
+      setIsEditing(false);
+    }
+  };
+
+  useEffect(() => {
+    if (workflow.name) {
+      setName(workflow.name);
+    }
+  }, [workflow.name]);
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
+  }, [isEditing]);
+
+  if (isEditing) {
+    return (
+      <Input
+        value={name}
+        ref={inputRef}
+        disabled={updateWorkflowName.isPending}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        onBlur={handleSave}
+        onKeyDown={handleKeyDown}
+        className="h-7 w-auto min-x-[100px] px-2"
+      />
+    );
+  }
+
+  return (
+    <BreadcrumbItem
+      className="cursor-pointer hover:text-foreground transition-colors"
+      onClick={() => {
+        setIsEditing(true);
+      }}
+    >
+      {workflow.name}
+    </BreadcrumbItem>
+  );
+};
+
+export const EditorBreadcrumbs = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/workflows" prefetch>
+              Workflows
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <RenameEditor workflowId={workflowId} />
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export const EditorSaveButton = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <div className="ml-auto">
+      <Button size="sm" onClick={() => {}} disabled={false}>
+        <SaveIcon className="size-4" /> Save
+      </Button>
+    </div>
+  );
+};
+
+const EditorHeader = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <header className="flex h-14 shrink-0 items-center gap-2 border-b px-4 bg-background">
+      <SidebarTrigger />
+      <div className="flex flex-row items-center justify-between gap-x-4 w-full">
+        <EditorBreadcrumbs workflowId={workflowId} />
+        <EditorSaveButton workflowId={workflowId} />
+      </div>
+    </header>
+  );
+};
+
+export default EditorHeader;
---
+++ src/features/editor/components/editor.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ErrorView, LoadingView } from "@/components/entity-components";
+import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+
+export const EditorLoading = () => {
+  return <LoadingView message="Hold On! We are loading your editor." />;
+};
+
+export const EditorError = () => {
+  return <ErrorView message="Some error occured while loading your editor." />;
+};
+
+export const Editor = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+  return <p>{JSON.stringify(workflow, null, 2)}</p>;
+};
